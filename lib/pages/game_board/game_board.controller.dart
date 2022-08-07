4fe3666062e5af17f22b4f09ac4b2ea949a91cd8
Florian Schmitz <schmitz.florian.daniel+github@gmail.com>
2022-08-07 02:26:53 +0200

    it works

--- lib/pages/game_board/game_board.controller.dart
+++ lib/pages/game_board/game_board.controller.dart
@@ -14,7 +14,7 @@ class PlaceController extends StateNotifier<List<GameBoardFieldModel>> {
     buildGameBoard();
   }
 
-  static final Logger _log = Logger('game_board.controller.dart');
+  final Logger _log = Logger('game_board.controller.dart');
 
   void buildGameBoard() async {
     final gameBoardFields = List<GameBoardFieldModel>.generate(
@@ -29,10 +29,22 @@ class PlaceController extends StateNotifier<List<GameBoardFieldModel>> {
 
   void setCoinOnGameField({required int index}) {
     _log.info('Setting coin on game field $index');
-    final gameBoardField = state[index];
+    int bottomFieldIndex = index;
+
+    // calculate the index of the bottom field, which is the field with the lowest index that is not empty
+    for (var i = index; i <= state.length - 1; i += 7) {
+      if (state[i].status == Status.empty) {
+        bottomFieldIndex = i;
+      }
+    }
+
+    _log.info('Setting coin on index: $bottomFieldIndex');
+    final gameBoardField = state[bottomFieldIndex];
     final newGameBoardField = gameBoardField.copyWith(status: Status.filled);
+
+    // replace the old field with the new field
     state = state.map((gameBoardField) {
-      if (gameBoardField.index == index) {
+      if (gameBoardField.index == bottomFieldIndex) {
         return newGameBoardField;
       }
       return gameBoardField;
